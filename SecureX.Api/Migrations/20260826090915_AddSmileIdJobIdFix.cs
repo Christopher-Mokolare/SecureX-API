d7237787231f8f19c99093ee76698bf9fd06e278
OBAKENG <2co.mokolare@gmail.com>
2026-08-26 11:09:47 +0200

    fix: add smile_id_job_id column migration

---
+++ SecureX.Api/Migrations/20260826090915_AddSmileIdJobIdFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureX.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSmileIdJobIdFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DO $$ BEGIN
+                    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='users' AND column_name='smile_id_job_id') THEN
+                        ALTER TABLE users ADD COLUMN smile_id_job_id text NULL;
+                    END IF;
+                END $$;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
